bf2cdcd776ae52952c6387d52b86fe9f23f8dbe8
NARUP0 <nrupgohil07@gmail.com>
2024-07-17 22:05:01 +0530

    linked list

---
+++ LinkedList_Creation.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data; // creating structure for linked list.
+    struct node *next;
+};
+
+struct node *creation(int n)
+{
+    struct node *head, *frr, *temp;
+
+    head = (struct node *)malloc(sizeof(struct node));
+    frr = head;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the %dth Element : ", i + 1);
+        scanf("%d", &head->data); // function for taking input in linked list
+        if (i == n - 1)
+        {
+            head->next = NULL;
+        }
+        else
+        {
+            temp = (struct node *)malloc(sizeof(struct node));
+            head->next = temp;
+            head = temp;
+        }
+    }
+    return (frr);
+}
+
+fre(struct node *frr)
+{
+    struct node *a = frr;
+    while (frr != NULL) // freeing the allocate memory
+    {
+        a = frr->next;
+        free(frr);
+        frr = a;
+    }
+}
+
+main()
+{
+    int n;
+    struct node *frr;
+    printf("Enter the no. of input : "); // taking no. of input
+    scanf("%d", &n);
+    frr = creation(n);
+    fre(frr);
+}
