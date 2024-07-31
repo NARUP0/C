8c70307f97215e2dd52db2c3d456bbd5f1bcec5e
NARUP0 <nrupgohil07@gmail.com>
2024-07-31 22:24:10 +0530

    bubble sort ascending

---
+++ AsceBubble_Sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void printarray(int * arr , int n)                      //function for printing array
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+void bubblesort(int * arr , int n)                     //function for sorting array with bubble sort but this code is not adaptive.
+{
+    int temp ;
+    
+    for (int i = 0; i < n-1; i++)
+    {
+        printf("completing %d pass\n",i+1);              //printing no. of passes.
+
+        for (int j = 0; j < n-1-i; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp ;
+            }
+        }
+        
+    }
+    
+}
+
+int main()
+{                   
+    int arr[]={6, 2, 9, 5, 3};                    //creating integer array.
+    int n = 5 ;                                   //setting size.
+    printarray(arr, n);
+    bubblesort(arr, n);
+    printarray(arr, n);
+
+    
+    return 0;
+}
---
+++ AsceBubble_SortAdaptive.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void printarray(int * arr , int n)                      //function for printing array
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+void bubblesort(int * arr , int n)                     //function for sorting array with bubble sort but this code is not adaptive.
+{
+    int temp ,issort ;
+    for (int i = 0; i < n-1; i++)
+    {   
+        issort = 1;
+
+        printf("completing %d pass\n",i+1);            //printing the no. of passes.
+
+        for (int j = 0; j < n-1-i; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp ;
+                issort = 0;                       // if the value of issort remains 1 it means the entire pass is not been compare 
+            }                                     //and it means the array has been sorted.
+        }
+        if(issort)
+        {
+            return ;
+        }
+        
+    }
+    
+}
+
+int main()
+{                   
+    // int arr[]={1, 25, 36, 47, 27, 50};
+    int arr[]={1, 2, 3, 4, 5, 6};                                     //creating integer array.
+    int n = 6 ;                                                             //setting size.
+    printarray(arr, n);
+    bubblesort(arr, n);
+    printarray(arr, n);
+
+    
+    return 0;
+}
